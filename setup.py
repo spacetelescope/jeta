a30c809336c5b6856446ef1321b299ac13b77d52
David Kauffman <dkauffman@stsci.edu>
2019-01-23 14:01:54 -0500

    mod: setup and versioning infomation.

--- setup.py
+++ setup.py
@@ -15,19 +15,19 @@ except ImportError:
 package_version.write_git_version_file()
 
 
-setup(name='jSka.jeta',
+setup(name='jeta',
       author='David Kauffman',
       description='Modules supporting jSka engineering telemetry archive',
       author_email='dkauffman@stsci.edu',
-      entry_points={'console_scripts': ['ska_fetch = Ska.engarchive.get_telem:main']},
-      py_modules=['jSka.jeta.archive.fetch', 'Ska.jeta.archive.converters', 'Ska.jeta.archive.utils',
-                  'Ska.jeta.archive.get_telem'],
+      entry_points={'console_scripts': ['jska_fetch = jeta.archive.get_telem:main']},
+      py_modules=['jeta.archive.fetch', 'jeta.archive.converters', 'jeta.archive.utils',
+                  'jeta.archive.get_telem'],
       version=package_version.version,
       zip_safe=False,
-      packages=['Ska', 'Ska.jeta', 'Ska.jeta.derived', 'Ska.jeta.tests'],
-      package_dir={'Ska': 'Ska'},
-      package_data={'Ska.jeta': ['*.dat', 'units_*.pkl', 'GIT_VERSION'],
-                    'Ska.jeta.tests': ['*.dat']},
+      packages=['jeta', 'jeta.archive.derived', 'jeta.tests'],
+      package_dir={'jeta': 'jeta'},
+      package_data={'jeta': ['*.dat', 'units_*.pkl', 'GIT_VERSION'],
+                    'jeta.tests': ['*.dat']},
       tests_require=['pytest'],
       cmdclass=cmdclass,
       )
