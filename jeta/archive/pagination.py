7fc7fcec156e3088105170f0609acac0145d7cd7
David Kauffman <dkauffman@stsci.edu>
2021-08-16 13:23:44 -0400

    wip raven full res fix

--- jeta/archive/pagination.py
+++ jeta/archive/pagination.py
@@ -1,31 +1,16 @@
 import numpy as np
 from jeta.archive import fetch
 
-def paginate_stat(msid, draw, idx0=0, idx1=None, interval='5min'):
+def paginate(msid, draw, idx0=0, length=10):
     """ A simple function to return a page of the full resolution data.
     """
-
-    results = fetch.MSID(msid, interval=interval)
-
-    times = results.times[idx0:idx0 + idx1]
-    values = results.vals[idx0:idx0 + idx1]
-
-    return {
-        'draw': draw,
-        'data': [times, values],
-        'recordsTotal': len(results),
-        'recordsFiltered': len(times)
-    }
-
-
-def paginate(msid, draw, idx0=0, idx1=None):
-    """ A simple function to return a page of the full resolution data.
-    """
-
     results = fetch.MSID(msid)
 
-    times = results.times[idx0:idx0 + idx1]
-    values = results.vals[idx0:idx0 + idx1]
+    start = idx0
+    stop = idx0 + length
+
+    times = results.times[start:stop]
+    values = results.vals[start:stop]
 
     return {
         'draw': draw,
--- jeta/version.py
+++ jeta/version.py
@@ -34,7 +34,7 @@ import os
 ### SET THESE VALUES
 ############################
 # Major, Minor, Bugfix, Dev
-VERSION = (2, 8, 1, False)
+VERSION = (2, 8, 3, False)
 
 
 class SemanticVersion(object):
