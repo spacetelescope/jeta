31afa3bc5dad4e15a5c35a206f2a470ac2257ba9
David Kauffman <dkauffman@stsci.edu>
2021-11-01 13:42:08 -0400

    bug: fixed issue getting msid archive coverage

--- jeta/version.py
+++ jeta/version.py
@@ -34,7 +34,7 @@ import os
 ### SET THESE VALUES
 ############################
 # Major, Minor, Bugfix, Dev
-VERSION = (2, 11, 0, False)
+VERSION = (2, 11, 1, False)
 
 
 class SemanticVersion(object):
